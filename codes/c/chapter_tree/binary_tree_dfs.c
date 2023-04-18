158bb41c72d8f009e263c790f22b5e370d9ab05d
andrea <andrea.hb.ji@outlook.com>
2023-04-18 21:30:12 +0800

    update: No longer using a global auxiliary array.

--- codes/c/chapter_tree/binary_tree_dfs.c
+++ codes/c/chapter_tree/binary_tree_dfs.c
@@ -2,43 +2,74 @@
  * File: binary_tree_dfs.c
  * Created Time: 2023-01-11
  * Author: Reanon (793584285@qq.com)
+ * Update by: AndreaJi (andrea.hb.ji@outlook.com)
  */
 
 #include "../include/include.h"
 
-/* 辅助数组，用于存储遍历序列 */
-int *arr;
-
-/* 前序遍历 */
-void preOrder(TreeNode *root, int *size) {
-    if (root == NULL) return;
+static int *pre_order_internel(TreeNode *root, int *size, int *arr)
+{
+    /* 辅助数组，用于存储遍历序列 */
+    if (arr == NULL)
+        arr = (int *)malloc(sizeof(int) * MAX_NODE_SIZE);
+    if (root == NULL)
+        return arr;
     // 访问优先级：根节点 -> 左子树 -> 右子树
     arr[(*size)++] = root->val;
-    preOrder(root->left, size);
-    preOrder(root->right, size);
+    pre_order_internel(root->left, size, arr);
+    pre_order_internel(root->right, size, arr);
+    return arr;
+}
+
+/* 前序遍历 */
+int *preOrder(TreeNode *root, int *size)
+{
+    return pre_order_internel(root, size, NULL);
+}
+
+static int *in_order_internel(TreeNode *root, int *size, int *arr)
+{
+    /* 辅助数组，用于存储遍历序列 */
+    if (arr == NULL)
+        arr = (int *)malloc(sizeof(int) * MAX_NODE_SIZE);
+    if (root == NULL)
+        return arr;
+    // 访问优先级：左子树 -> 根节点 -> 右子树
+    in_order_internel(root->left, size, arr);
+    arr[(*size)++] = root->val;
+    in_order_internel(root->right, size, arr);
+    return arr;
 }
 
 /* 中序遍历 */
-void inOrder(TreeNode *root, int *size) {
-    if (root == NULL) return;
-    // 访问优先级：左子树 -> 根节点 -> 右子树
-    inOrder(root->left, size);
+int *inOrder(TreeNode *root, int *size)
+{
+    return in_order_internel(root, size, NULL);
+}
+
+static int *post_order_internel(TreeNode *root, int *size, int *arr)
+{
+    /* 辅助数组，用于存储遍历序列 */
+    if (arr == NULL)
+        arr = (int *)malloc(sizeof(int) * MAX_NODE_SIZE);
+    if (root == NULL)
+        return arr;
+    // 访问优先级：左子树 -> 右子树 -> 根节点
+    post_order_internel(root->left, size, arr);
+    post_order_internel(root->right, size, arr);
     arr[(*size)++] = root->val;
-    inOrder(root->right, size);
+    return arr;
 }
 
 /* 后序遍历 */
-void postOrder(TreeNode *root, int *size) {
-    if (root == NULL) return;
-    // 访问优先级：左子树 -> 右子树 -> 根节点
-    postOrder(root->left, size);
-    postOrder(root->right, size);
-    arr[(*size)++] = root->val;
+int *postOrder(TreeNode *root, int *size)
+{
+    return post_order_internel(root, size, NULL);
 }
 
-
 /* Driver Code */
-int main() {
+int main()
+{
     /* 初始化二叉树 */
     // 这里借助了一个从数组直接生成二叉树的函数
     int nums[] = {1, 2, 3, 4, 5, 6, 7};
@@ -47,25 +78,27 @@ int main() {
     printf("初始化二叉树\n");
     printTree(root);
 
+    int *arr;
     /* 前序遍历 */
-    // 初始化辅助数组
-    arr = (int *) malloc(sizeof(int) * MAX_NODE_SIZE);
     size = 0;
-    preOrder(root, &size);
+    arr = preOrder(root, &size);
     printf("前序遍历的节点打印序列 = ");
     printArray(arr, size);
+    free(arr);
 
     /* 中序遍历 */
     size = 0;
-    inOrder(root, &size);
+    arr = inOrder(root, &size);
     printf("中序遍历的节点打印序列 = ");
     printArray(arr, size);
+    free(arr);
 
     /* 后序遍历 */
     size = 0;
-    postOrder(root, &size);
+    arr = postOrder(root, &size);
     printf("后序遍历的节点打印序列 = ");
     printArray(arr, size);
+    free(arr);
 
     return 0;
 }
